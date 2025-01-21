bc234431084b63943693af5f739274792c0e6d3b
Ranbir Deol <ranbirs.deol17@gmail.com>
2025-01-21 10:34:24 -0500

    Remove frontend bloat

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,21 @@
+// Backend
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
 // Initialize the app
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
 // Middleware
 app.use(cors()); // Enable Cross-Origin Resource Sharing
 app.use(bodyParser.json()); // Parse JSON bodies
 app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
-// Sample data (mock database)
-const pokemons = [
-    { id: 1, name: 'Bulbasaur', type: 'Grass/Poison' },
-    { id: 2, name: 'Charmander', type: 'Fire' },
-    { id: 3, name: 'Squirtle', type: 'Water' },
-];
-
 // Routes
 app.get('/', (req, res) => {
-    res.send('Welcome to the backend server!');
+    res.send('Backend API is Running');
 });
 
 // Start the server
