08a59e0df5961feaeb63683f6b78cd76398a7df2
RanbirSDeol <ranbirs.deol17@gmail.com>
2025-01-21 20:05:49 -0500

    Implement home routing, and home component layout.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+
 function App() {
   return (
-    <div className="App">
-      <p>hello world</p>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Home route to our home component */}
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
